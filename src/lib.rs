09cb664d802c38ddf0f25a5790daa588f8cfa348
Michail Denchev <mdenchev@gmail.com>
2022-03-10 23:57:58 -0500

    Refactor

--- src/lib.rs
+++ src/lib.rs
@@ -1,23 +1,7 @@
-#![deny(
-    missing_docs,
-    non_camel_case_types,
-    non_snake_case,
-    path_statements,
-    trivial_casts,
-    trivial_numeric_casts,
-    unsafe_code,
-    unused_allocation,
-    unused_import_braces,
-    unused_imports,
-    unused_must_use,
-    unused_mut,
-    while_true,
-    array_into_iter,
-    clippy::panic,
-    clippy::print_stdout,
-    clippy::todo,
-)]
+#![allow(clippy::type_complexity)]
 #![doc = include_str!("../README.MD")]
+
+mod loader;
 
 use std::ops::DerefMut;
 use std::path::{Path, PathBuf};
@@ -25,7 +9,7 @@ use std::path::{Path, PathBuf};
 use aseprite_reader::{Aseprite, AsepriteSliceImage, NineSlice};
 use bevy::prelude::*;
 use bevy::reflect::TypeUuid;
-use bevy::render::render_resource::{TextureFormat, Extent3d, TextureDimension};
+use bevy::render::render_resource::{Extent3d, TextureDimension, TextureFormat};
 use bevy::{
     asset::{AssetLoader, AssetServerSettings, LoadedAsset},
     utils::HashMap,
@@ -33,6 +17,7 @@ use bevy::{
 
 use bevy::sprite::TextureAtlasBuilder;
 pub use bevy_spicy_aseprite_derive::aseprite;
+use loader::{check_aseprite_data, load_aseprites, AsepriteLoader};
 
 /// The required plugin to fully use your aseprite files
 pub struct AsepritePlugin;
@@ -46,23 +31,11 @@ impl Plugin for AsepritePlugin {
     fn build(&self, app: &mut bevy::prelude::App) {
         app.add_asset::<AsepriteImage>()
             .add_asset_loader(AsepriteLoader)
-            .add_system(
-                check_aseprite_data
-                    .before(AsepriteSystems::UpdateAnim),
-            )
+            .add_system(check_aseprite_data.before(AsepriteSystems::UpdateAnim))
             .add_system(load_aseprites)
-            .add_system(
-                switch_tag
-                    .before(AsepriteSystems::UpdateAnim),
-            )
-            .add_system(
-                update_animations
-                    .label(AsepriteSystems::UpdateAnim),
-            )
-            .add_system(
-                update_spritesheet_anim
-                    .after(AsepriteSystems::UpdateAnim),
-            );
+            .add_system(switch_tag.before(AsepriteSystems::UpdateAnim))
+            .add_system(update_animations.label(AsepriteSystems::UpdateAnim))
+            .add_system(update_spritesheet_anim.after(AsepriteSystems::UpdateAnim));
     }
 }
 
@@ -126,19 +99,21 @@ fn update_animations(
 
 fn switch_tag(
     aseprite_image_assets: Res<Assets<AsepriteImage>>,
-    mut aseprites_query: Query<(
-        &Handle<AsepriteImage>,
-        &AsepriteAnimation,
-        &mut AsepriteAnimationState,
-    ), Changed<AsepriteAnimation>>,
+    mut aseprites_query: Query<
+        (
+            &Handle<AsepriteImage>,
+            &AsepriteAnimation,
+            &mut AsepriteAnimationState,
+        ),
+        Changed<AsepriteAnimation>,
+    >,
 ) {
     for (aseprite_handle, aseprite_animation, mut aseprite_animation_state) in
         aseprites_query.iter_mut()
     {
-        let image = if let Some(image) = aseprite_image_assets.get(aseprite_handle.clone_weak()) {
-            image
-        } else {
-            continue;
+        let image = match aseprite_image_assets.get(aseprite_handle) {
+            Some(image) => image,
+            None => continue,
         };
 
         let (current_frame_idx, _forward, rest_time) = match &mut *aseprite_animation_state {
@@ -155,25 +130,7 @@ fn switch_tag(
     }
 }
 
-fn load_aseprites(
-    mut commands: Commands,
-    asset_server_settings_folder: Res<AssetServerSettings>,
-    asset_server: Res<AssetServer>,
-    new_aseprites: Query<(Entity, &AsepriteInfo), Added<AsepriteInfo>>,
-) {
-    for (entity, ase_info) in new_aseprites.iter() {
-        let path = ase_info
-            .path()
-            .strip_prefix(&asset_server_settings_folder.asset_folder)
-            .unwrap();
-
-        let handle: Handle<AsepriteImage> = asset_server.load(path);
-
-        commands.entity(entity).insert(handle);
-    }
-}
-
-// Update the 
+// Update the
 fn update_spritesheet_anim(
     aseprite_assets: Res<Assets<AsepriteImage>>,
     texture_atlas_assets: Res<Assets<TextureAtlas>>,
@@ -227,101 +184,7 @@ fn update_spritesheet_anim(
 }
 
 #[derive(Component)]
-struct AsepriteSheetEntity(Entity);
-
-fn check_aseprite_data(
-    mut commands: Commands,
-    mut aseprite_image_events: EventReader<AssetEvent<AsepriteImage>>,
-    mut aseprite_image_assets: ResMut<Assets<AsepriteImage>>,
-    mut texture_assets: ResMut<Assets<Image>>,
-    mut texture_atlas_assets: ResMut<Assets<TextureAtlas>>,
-    mut existing_aseprites: Query<
-        (
-            Entity,
-            Option<&AsepriteSheetEntity>,
-            &mut Handle<AsepriteImage>,
-        ),
-        With<AsepriteAnimation>,
-    >,
-) {
-    for event in aseprite_image_events.iter() {
-        match event {
-            AssetEvent::Created { handle } | AssetEvent::Modified { handle } => {
-                {
-                    if let Some(img) = aseprite_image_assets.get(handle) {
-                        if img.atlas.is_handle() {
-                            continue;
-                        }
-                    } else {
-                        continue;
-                    }
-                }
-
-                let image = if let Some(img) = aseprite_image_assets.get_mut(handle) {
-                    img
-                } else {
-                    continue;
-                };
-
-                image
-                    .atlas
-                    .load(&mut texture_atlas_assets, &mut texture_assets);
-
-                for (aseprite_entity, aseprite_sheet, mut aseprite_handle) in
-                    existing_aseprites.iter_mut()
-                {
-                    if &*aseprite_handle != handle {
-                        info!("Not the same handle");
-                        continue;
-                    }
-
-                    let image = if let Some(image) = aseprite_image_assets.get(&*aseprite_handle) {
-                        image
-                    } else {
-                        info!("Not aseprite image");
-                        continue;
-                    };
-
-                    let atlas_handle = if let Some(atlas_handle) = image.atlas.get_atlas() {
-                        atlas_handle
-                    } else {
-                        info!("No texture atlas");
-                        continue;
-                    };
-
-                    // Pretend we updated the handle, so we can listen to changes
-                    aseprite_handle.deref_mut();
-
-                    let sheet_entity = match &aseprite_sheet {
-                        Some(AsepriteSheetEntity(entity)) => *entity,
-                        None => {
-                            let entity = commands
-                                .spawn_bundle(SpriteSheetBundle {
-                                    texture_atlas: atlas_handle.clone(),
-                                    ..Default::default()
-                                })
-                                .id();
-
-                            commands
-                                .entity(aseprite_entity)
-                                .push_children(&[entity.clone()])
-                                .insert(AsepriteSheetEntity(entity));
-
-                            entity
-                        }
-                    };
-
-                    commands
-                        .entity(sheet_entity)
-                        .insert(TextureAtlasSprite::new(0));
-
-                    info!("Finished inserting entities for aseprite");
-                }
-            }
-            AssetEvent::Removed { .. } => (),
-        }
-    }
-}
+pub(crate) struct AsepriteSheetEntity(Entity);
 
 /// A tag representing an animation
 #[derive(Debug, Default, Copy, Clone, PartialEq, Eq)]
@@ -342,7 +205,7 @@ impl AsepriteTag {
     }
 }
 
-/// A tag representing an animation
+// TODO I don't think this is used anywhere currently.
 #[derive(Debug, Default, Copy, Clone)]
 pub struct AsepriteSlice(&'static str);
 
@@ -431,124 +294,6 @@ impl AsepriteImage {
     }
 }
 
-/// The loader of aseprite files
-#[derive(Debug, Default)]
-pub struct AsepriteLoader;
-
-impl AssetLoader for AsepriteLoader {
-    fn load<'a>(
-        &'a self,
-        bytes: &'a [u8],
-        load_context: &'a mut bevy::asset::LoadContext,
-    ) -> bevy::asset::BoxedFuture<'a, Result<(), anyhow::Error>> {
-        Box::pin(async move {
-            info!("Loading aseprite at {:?}", load_context.path());
-            let aseprite = Aseprite::from_bytes(bytes)?;
-
-            let frames = aseprite.frames();
-            let images = frames
-                .get_for(&(0..frames.count() as u16))
-                .get_images()
-                .unwrap();
-
-            let mut aseprite_atlas = TextureAtlasBuilder::default()
-                .format(TextureFormat::Rgba8UnormSrgb);
-
-            let mut frame_textures = vec![];
-
-            for (idx, image) in images.into_iter().enumerate() {
-                let texture = Image::new(
-                    Extent3d { width: image.width(), height: image.height(), depth_or_array_layers: 1 },
-                    TextureDimension::D2,
-                    image.into_raw(),
-                    TextureFormat::Rgba8UnormSrgb,
-                );
-                let label = format!("Frame{}", idx);
-                let texture_handle =
-                    load_context.set_labeled_asset(&label, LoadedAsset::new(texture.clone()));
-
-                aseprite_atlas.add_texture(texture_handle.clone(), &texture);
-                frame_textures.push(texture_handle);
-            }
-
-            info!("Finished loading aseprite");
-
-            let slices = aseprite.slices();
-
-            let slice_textures = slices
-                .get_all()
-                .map(|slice| slice.name.clone())
-                .zip(
-                    slices
-                        .get_images(slices.get_all())?
-                        .into_iter()
-                        .zip(slices.get_all())
-                        .map(|(AsepriteSliceImage { image, nine_slices }, slice)| {
-                            let texture = Image::new(
-                                Extent3d { width: image.width(), height: image.height(), depth_or_array_layers: 1 },
-                                TextureDimension::D2,
-                                image.into_raw(),
-                                TextureFormat::Rgba8UnormSrgb,
-                            );
-
-                            let label = slice.label();
-                            let texture_handle = load_context
-                                .set_labeled_asset(&label, LoadedAsset::new(texture.clone()));
-
-                            aseprite_atlas.add_texture(texture_handle.clone(), &texture);
-
-                            let nine_patch_handles = nine_slices.map(|nine_slices| {
-                                nine_slices
-                                    .into_iter()
-                                    .map(|(key, image_buffer)| {
-                                        let texture = Image::new(
-                                            Extent3d {
-                                                width: image_buffer.width(),
-                                                height: image_buffer.height(),
-                                                depth_or_array_layers: 1,
-                                            },
-                                            TextureDimension::D2,
-                                            image_buffer.into_raw(),
-                                            TextureFormat::Rgba8UnormSrgb,
-                                        );
-
-                                        let label = slice.label_with_nine_slice(key);
-                                        let texture_handle = load_context.set_labeled_asset(
-                                            &label,
-                                            LoadedAsset::new(texture.clone()),
-                                        );
-
-                                        aseprite_atlas
-                                            .add_texture(texture_handle.clone(), &texture);
-
-                                        (key, texture_handle)
-                                    })
-                                    .collect()
-                            });
-
-                            AsepriteSliceTextures {
-                                texture_handle,
-                                nine_patch_handles,
-                            }
-                        }),
-                )
-                .collect();
-
-            load_context.set_default_asset(LoadedAsset::new(AsepriteImage {
-                aseprite,
-                atlas: Atlas::Builder(aseprite_atlas),
-                frames: frame_textures,
-                slices: slice_textures,
-            }));
-            Ok(())
-        })
-    }
-
-    fn extensions(&self) -> &[&str] {
-        &["ase", "aseprite"]
-    }
-}
-
 /// All the info about a specific aseprite
 #[derive(Debug, Default, Component)]
 pub struct AsepriteInfo {
@@ -562,6 +307,8 @@ impl AsepriteInfo {
     }
 }
 
+// TODO this is not a great way to store this info
+// Should combine with AsepriteAnimationState
 #[derive(Debug, Component, PartialEq, Eq)]
 /// An aseprite animation
 pub enum AsepriteAnimation {
@@ -582,25 +329,24 @@ impl Default for AsepriteAnimation {
 
 impl AsepriteAnimation {
     /// Return the first frame of this tag
-    pub fn get_first_frame(&self,
-        aseprite: &AsepriteImage,
-    ) -> usize {
+    pub fn get_first_frame(&self, aseprite: &AsepriteImage) -> usize {
         match self {
             AsepriteAnimation::Tag {
                 tag: AsepriteTag(name),
             } => {
                 let tags = aseprite.aseprite.tags();
-                let tag = if let Some(tag) = tags.get_by_name(name) {
-                    tag
-                } else {
-                    error!("Tag {} wasn't found.", name);
-                    return 0
+                let tag = match tags.get_by_name(name) {
+                    Some(tag) => tag,
+                    None => {
+                        error!("Tag {} wasn't found.", name);
+                        return 0;
+                    }
                 };
 
                 let range = tag.frames.clone();
                 range.start as usize
             }
-            _ => 0
+            _ => 0,
         }
     }
 
@@ -616,11 +362,12 @@ impl AsepriteAnimation {
                 tag: AsepriteTag(name),
             } => {
                 let tags = aseprite.aseprite.tags();
-                let tag = if let Some(tag) = tags.get_by_name(name) {
-                    tag
-                } else {
-                    error!("Tag {} wasn't found.", name);
-                    return (0, false);
+                let tag = match tags.get_by_name(name) {
+                    Some(tag) => tag,
+                    None => {
+                        error!("Tag {} wasn't found.", name);
+                        return (0, false);
+                    }
                 };
 
                 let range = tag.frames.clone();
@@ -790,23 +537,16 @@ impl Default for AsepriteAnimationState {
     }
 }
 
-#[derive(Debug, Default, Component)]
-/// Defines if this aseprite should be treated as a grid
-pub struct AsepriteGrid {
-    padding: Option<u64>,
-}
-
 /// A bundle defining a drawn aseprite
 #[derive(Debug, Bundle, Default)]
-#[allow(missing_docs)]
 pub struct AsepriteBundle {
     pub transform: Transform,
     pub global_transform: GlobalTransform,
+    // TODO what's the point of this?
     pub aseprite: AsepriteInfo,
     pub animation: AsepriteAnimation,
     pub animation_state: AsepriteAnimationState,
     pub handle: Handle<AsepriteImage>,
-    pub grid_info: AsepriteGrid,
 }
 
 /// Helper methods to get the label for a specific slice
---
+++ src/loader.rs
@@ -0,0 +1,248 @@
+use aseprite_reader::{Aseprite, AsepriteSliceImage};
+use bevy::{asset::{AssetLoader, LoadedAsset, AssetServerSettings}, prelude::*, render::render_resource::{Extent3d, TextureDimension, TextureFormat}};
+
+use crate::{AsepriteSliceName, AsepriteSliceTextures, AsepriteImage, Atlas, AsepriteInfo, AsepriteSheetEntity, AsepriteAnimation};
+
+
+#[derive(Debug, Default)]
+pub struct AsepriteLoader;
+
+impl AssetLoader for AsepriteLoader {
+    fn load<'a>(
+        &'a self,
+        bytes: &'a [u8],
+        load_context: &'a mut bevy::asset::LoadContext,
+    ) -> bevy::asset::BoxedFuture<'a, Result<(), anyhow::Error>> {
+        Box::pin(async move {
+            info!("Loading aseprite at {:?}", load_context.path());
+            let aseprite = Aseprite::from_bytes(bytes)?;
+
+            let frames = aseprite.frames();
+            let images = frames
+                .get_for(&(0..frames.count() as u16))
+                .get_images()
+                .unwrap();
+
+            let mut aseprite_atlas = TextureAtlasBuilder::default();
+
+            let mut frame_textures = vec![];
+
+            for (idx, image) in images.into_iter().enumerate() {
+                let texture = Image::new(
+                    Extent3d {
+                        width: image.width(),
+                        height: image.height(),
+                        depth_or_array_layers: 1,
+                    },
+                    TextureDimension::D2,
+                    image.into_raw(),
+                    TextureFormat::Rgba8UnormSrgb,
+                );
+                let label = format!("Frame{}", idx);
+                let texture_handle =
+                    load_context.set_labeled_asset(&label, LoadedAsset::new(texture.clone()));
+
+                aseprite_atlas.add_texture(texture_handle.clone(), &texture);
+                frame_textures.push(texture_handle);
+            }
+
+            info!("Finished loading aseprite");
+
+            let slices = aseprite.slices();
+
+            let slice_textures = slices
+                .get_all()
+                .map(|slice| slice.name.clone())
+                .zip(
+                    slices
+                        .get_images(slices.get_all())?
+                        .into_iter()
+                        .zip(slices.get_all())
+                        .map(|(AsepriteSliceImage { image, nine_slices }, slice)| {
+                            let texture = Image::new(
+                                Extent3d {
+                                    width: image.width(),
+                                    height: image.height(),
+                                    depth_or_array_layers: 1,
+                                },
+                                TextureDimension::D2,
+                                image.into_raw(),
+                                TextureFormat::Rgba8UnormSrgb,
+                            );
+
+                            let label = slice.label();
+                            let texture_handle = load_context
+                                .set_labeled_asset(&label, LoadedAsset::new(texture.clone()));
+
+                            aseprite_atlas.add_texture(texture_handle.clone(), &texture);
+
+                            let nine_patch_handles = nine_slices.map(|nine_slices| {
+                                nine_slices
+                                    .into_iter()
+                                    .map(|(key, image_buffer)| {
+                                        let texture = Image::new(
+                                            Extent3d {
+                                                width: image_buffer.width(),
+                                                height: image_buffer.height(),
+                                                depth_or_array_layers: 1,
+                                            },
+                                            TextureDimension::D2,
+                                            image_buffer.into_raw(),
+                                            TextureFormat::Rgba8UnormSrgb,
+                                        );
+
+                                        let label = slice.label_with_nine_slice(key);
+                                        let texture_handle = load_context.set_labeled_asset(
+                                            &label,
+                                            LoadedAsset::new(texture.clone()),
+                                        );
+
+                                        aseprite_atlas
+                                            .add_texture(texture_handle.clone(), &texture);
+
+                                        (key, texture_handle)
+                                    })
+                                    .collect()
+                            });
+
+                            AsepriteSliceTextures {
+                                texture_handle,
+                                nine_patch_handles,
+                            }
+                        }),
+                )
+                .collect();
+
+            load_context.set_default_asset(LoadedAsset::new(AsepriteImage {
+                aseprite,
+                atlas: Atlas::Builder(aseprite_atlas),
+                frames: frame_textures,
+                slices: slice_textures,
+            }));
+            Ok(())
+        })
+    }
+
+    fn extensions(&self) -> &[&str] {
+        &["ase", "aseprite"]
+    }
+}
+
+// TODO add support for hot reloading
+pub(crate) fn check_aseprite_data(
+    mut commands: Commands,
+    mut aseprite_image_events: EventReader<AssetEvent<AsepriteImage>>,
+    mut aseprite_image_assets: ResMut<Assets<AsepriteImage>>,
+    mut texture_assets: ResMut<Assets<Image>>,
+    mut texture_atlas_assets: ResMut<Assets<TextureAtlas>>,
+    mut existing_aseprites: Query<
+        (
+            Entity,
+            &Transform,
+            Option<&AsepriteSheetEntity>,
+            &mut Handle<AsepriteImage>,
+        ),
+        With<AsepriteAnimation>,
+    >,
+) {
+    for event in aseprite_image_events.iter() {
+        match event {
+            AssetEvent::Created { handle } => {
+                // Get the created/modified aseprite
+                let ase = match aseprite_image_assets.get_mut(handle) {
+                    Some(ase) => ase,
+                    None => continue,
+                };
+
+                // If the atlas is already built, don't do anything
+                if ase.atlas.is_handle() {
+                    continue;
+                }
+
+                // Build the TextureAtlas -- Assets<Image> is not available in the loader so do it here
+                ase.atlas
+                    .load(&mut texture_atlas_assets, &mut texture_assets);
+
+                // Check if the aseprite has previously been loaded -- ?
+                let existing_ase = existing_aseprites
+                    .iter_mut()
+                    .find(|(_, _, _, query_handle)| &**query_handle == handle);
+
+                let (ent, &transform, sheet, mut handle) = match existing_ase {
+                    Some(v) => v,
+                    None => continue,
+                };
+
+                // TODO Is this needed? Wouldn't the handle be invalid if the asset didn't exist?
+                let image = match aseprite_image_assets.get(&*handle) {
+                    Some(image) => image,
+                    None => {
+                        info!("Not aseprite image");
+                        continue;
+                    }
+                };
+
+                // TODO This also feel redundant as a similar check was done at the start of the function
+                let atlas_handle = match image.atlas.get_atlas() {
+                    Some(atlas_handle) => atlas_handle,
+                    None => {
+                        info!("No texture atlas");
+                        continue;
+                    }
+                };
+
+                // If no sprite, add create and add as child of current entity
+                let sheet_entity = match &sheet {
+                    Some(AsepriteSheetEntity(entity)) => *entity,
+                    None => {
+                        let entity = commands
+                            .spawn_bundle(SpriteSheetBundle {
+                                texture_atlas: atlas_handle.clone(),
+                                transform,
+                                ..Default::default()
+                            })
+                            .id();
+
+                        commands
+                            .entity(ent)
+                            //.push_children(&[entity])
+                            .insert(AsepriteSheetEntity(entity));
+
+                        entity
+                    }
+                };
+
+                commands
+                    .entity(sheet_entity)
+                    .insert(TextureAtlasSprite::new(0));
+
+                info!("Finished inserting entities for aseprite");
+            }
+            AssetEvent::Removed { .. } | AssetEvent::Modified { .. } => (),
+        }
+    }
+}
+
+
+// This is used so you don't need to load the aseprite when creating the bundle
+/* TODO maybe get rid of this? Though it does provide cool ergonomics as you
+    can create a bundle without an assetserver. But then again why wouldn't
+    you have an assetserver..
+*/
+pub(crate) fn load_aseprites(
+    mut commands: Commands,
+    asset_server_settings_folder: Res<AssetServerSettings>,
+    asset_server: Res<AssetServer>,
+    new_aseprites: Query<(Entity, &AsepriteInfo), Added<AsepriteInfo>>,
+) {
+    for (entity, ase_info) in new_aseprites.iter() {
+        let path = ase_info
+            .path()
+            .strip_prefix(&asset_server_settings_folder.asset_folder)
+            .unwrap();
+
+        let handle: Handle<AsepriteImage> = asset_server.load(path);
+
+        commands.entity(entity).insert(handle);
+    }
+}
